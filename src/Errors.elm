05f74f74e811e972925f52848430afee90d6984d
Rupert Smith <rupert@thesett.com>
2020-03-09 16:59:10 +0000

    Commented out markdown stuff for now - may use markup instead.

--- src/Errors.elm
+++ src/Errors.elm
@@ -1,12 +1,12 @@
 module Errors exposing (..)
 
 import Dict exposing (Dict)
-import Markdown.Block exposing (Block)
-import Markdown.Html
-import Markdown.Parser exposing (ListItem(..), Renderer)
 
 
 
+-- import Markdown.Block exposing (Block)
+-- import Markdown.Html
+-- import Markdown.Parser exposing (ListItem(..), Renderer)
 -- TODO:
 -- When converting errors, they can have multiple positions in them,
 -- so quoted code blocks must be a list, to allow for more than one.
@@ -18,7 +18,7 @@ type Error
     = Error
         { code : Int
         , title : String
-        , body : List Block
+        , body : List String
         }
 
 
@@ -43,41 +43,45 @@ type alias ErrorBuilder pos err =
 
 asConsoleString : Error -> String
 asConsoleString (Error { code, title, body }) =
-    let
-        bodyResult =
-            Markdown.Parser.render consoleRenderer body
-                |> Result.map (List.foldl (++) "")
-                |> Result.withDefault ""
-    in
+    -- let
+    --     bodyResult =
+    --         Markdown.Parser.render consoleRenderer body
+    --             |> Result.map (List.foldl (++) "")
+    --             |> Result.withDefault ""
+    -- in
+    -- String.fromInt code
+    --     ++ " : "
+    --     ++ title
+    --     ++ "\n\n"
+    --     ++ bodyResult
     String.fromInt code
         ++ " : "
         ++ title
-        ++ "\n\n"
-        ++ bodyResult
 
 
-consoleRenderer : Renderer String
-consoleRenderer =
-    { heading = \{ level, children, rawText } -> List.foldl (++) "" children
-    , raw = List.foldl (++) ""
-    , blockQuote = List.foldl (++) ""
-    , html = Markdown.Html.oneOf []
-    , plain = \content -> content
-    , code = \content -> "" ++ content
-    , bold = \content -> "" ++ content
-    , italic = \content -> "" ++ content
-    , link = \link content -> link.destination |> Ok
-    , image = \image content -> content |> Ok
-    , unorderedList =
-        \items ->
-            items
-                |> List.map (\(ListItem _ itemBlocks) -> List.foldl (++) "" itemBlocks)
-                |> List.foldl (++) ""
-    , orderedList =
-        \startingIndex items ->
-            items
-                |> List.map (List.foldl (++) "")
-                |> List.foldl (++) ""
-    , codeBlock = \{ body, language } -> "" ++ body
-    , thematicBreak = ""
-    }
+
+-- consoleRenderer : Renderer String
+-- consoleRenderer =
+--     { heading = \{ level, children, rawText } -> List.foldl (++) "" children
+--     , raw = List.foldl (++) ""
+--     , blockQuote = List.foldl (++) ""
+--     , html = Markdown.Html.oneOf []
+--     , plain = \content -> content
+--     , code = \content -> "" ++ content
+--     , bold = \content -> "" ++ content
+--     , italic = \content -> "" ++ content
+--     , link = \link content -> link.destination |> Ok
+--     , image = \image content -> content |> Ok
+--     , unorderedList =
+--         \items ->
+--             items
+--                 |> List.map (\(ListItem _ itemBlocks) -> List.foldl (++) "" itemBlocks)
+--                 |> List.foldl (++) ""
+--     , orderedList =
+--         \startingIndex items ->
+--             items
+--                 |> List.map (List.foldl (++) "")
+--                 |> List.foldl (++) ""
+--     , codeBlock = \{ body, language } -> "" ++ body
+--     , thematicBreak = ""
+--     }
