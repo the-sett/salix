5655e09c59827f446f01cfeb2c71c94cd7a774c7
Rupert Smith <rupert@thesett.com>
2020-07-13 21:33:17 +0100

    First pass at writing the encoder named ref function.

--- src/Elm/Json/NamedRef.elm
+++ src/Elm/Json/NamedRef.elm
@@ -2,34 +2,74 @@ module Elm.Json.NamedRef exposing (..)
 
 import Dict exposing (Dict)
 import Elm.CodeGen as CG exposing (Expression)
+import Enum exposing (Enum)
 import L2 exposing (L2, RefChecked)
 import L3 exposing (PropertiesAPI)
+import Naming
 import ResultME exposing (ResultME)
 
 
-decoderNamed : PropertiesAPI pos -> L2 pos -> String -> Expression
-decoderNamed propertiesApi model named =
-    case options.namedTypeDecoder of
-        AssumeCodec ->
-            CG.apply
-                [ CG.fqFun codecMod "decoder"
-                , CG.val (Naming.safeCCL (named ++ "Codec"))
-                ]
-                |> CG.parens
+jsonCodingEnum : Enum String
+jsonCodingEnum =
+    Enum.define
+        [ "Encoder"
+        , "Decoder"
+        , "MinibillCodec"
+        ]
+        identity
 
-        AssumeDecoder ->
-            CG.fun (Naming.safeCCL (named ++ "Decoder"))
+
+decoderNamed : PropertiesAPI pos -> L2 pos -> String -> ResultME String Expression
+decoderNamed propertiesApi model named =
+    -- case options.namedTypeDecoder of
+    --     AssumeCodec ->
+    --         CG.apply
+    --             [ CG.fqFun codecMod "decoder"
+    --             , CG.val (Naming.safeCCL (named ++ "Codec"))
+    --             ]
+    --             |> CG.parens
+    --
+    --     AssumeDecoder ->
+    --         CG.fun (Naming.safeCCL (named ++ "Decoder"))
+    Dict.get named model
+        |> Maybe.map
+            (\decl ->
+                (propertiesApi.declarable decl).getOptionalEnumProperty jsonCodingEnum "jsonCoding"
+                    |> ResultME.map
+                        (Maybe.map
+                            (\coding ->
+                                case coding of
+                                    "Decoder" ->
+                                        CG.fun (Naming.safeCCL (named ++ "Decoder"))
+
+                                    "MinibillCodec" ->
+                                        CG.apply
+                                            [ CG.fqFun codecMod "decoder"
+                                            , CG.val (Naming.safeCCL (named ++ "Codec"))
+                                            ]
+                                            |> CG.parens
+                            )
+                        )
+            )
+        --|> Maybe.withDefault (ResultME.error "Name not found in model.")
+        |> Maybe.withDefault (Ok CG.unit)
 
 
 encoderNamed : PropertiesAPI pos -> L2 pos -> String -> Expression
 encoderNamed propertiesApi model named =
-    case options.namedTypeEncoder of
-        AssumeCodec ->
-            CG.apply
-                [ CG.fqFun codecMod "encoder"
-                , CG.val (Naming.safeCCL (named ++ "Codec"))
-                ]
-                |> CG.parens
+    -- case options.namedTypeEncoder of
+    --     AssumeCodec ->
+    --         CG.apply
+    --             [ CG.fqFun codecMod "encoder"
+    --             , CG.val (Naming.safeCCL (named ++ "Codec"))
+    --             ]
+    --             |> CG.parens
+    --
+    --     AssumeEncoder ->
+    --         CG.fun (Naming.safeCCL (named ++ "Encoder"))
+    CG.unit
 
-        AssumeEncoder ->
-            CG.fun (Naming.safeCCL (named ++ "Encoder"))
+
+codecMod : List String
+codecMod =
+    [ "Codec" ]
