982ce5103bb448a9d27a2b46109e3a86026aa526
Rupert Smith <rupert@thesett.com>
2020-08-20 10:47:51 +0100

    Outline.

--- src/Salix/Pretty.elm
+++ src/Salix/Pretty.elm
@@ -4,7 +4,7 @@ module Salix.Pretty exposing (..)
 -}
 
 import Dict exposing (Dict)
-import L1 exposing (Basic(..), Container(..), Declarable(..), L1, Restricted(..), Type(..), Unchecked)
+import L1 exposing (Basic(..), Container(..), Declarable(..), L1, Restricted(..), Type(..))
 import List.Nonempty exposing (Nonempty(..))
 import Maybe.Extra
 import Pretty exposing (Doc)
@@ -16,3 +16,28 @@ import Set exposing (Set)
 prepareLayout : L1 pos -> Doc
 prepareLayout model =
     Pretty.string "blah"
+
+
+prettyBasic : Basic -> Doc
+prettyBasic _ =
+    Debug.todo "prettyBasic"
+
+
+prettyContainer : Container pos ref -> Doc
+prettyContainer _ =
+    Debug.todo "prettyContainer"
+
+
+prettyRestricted : Restricted -> Doc
+prettyRestricted _ =
+    Debug.todo "prettyRestricted"
+
+
+prettyType : Type pos ref -> Doc
+prettyType _ =
+    Debug.todo "prettyType"
+
+
+prettyDeclarable : Declarable pos ref -> Doc
+prettyDeclarable _ =
+    Debug.todo "prettyDeclarable"
